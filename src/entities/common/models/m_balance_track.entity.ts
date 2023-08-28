559bdb2a9739915f3e2152cac842249f0c09cc78
gokulvnu10 <132879587+gokulvnu10@users.noreply.github.com>
2023-08-28 14:56:57 +0530

    Fix

--- src/entities/common/models/m_balance_track.entity.ts
+++ src/entities/common/models/m_balance_track.entity.ts
@@ -1,4 +1,4 @@
-import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class M_Balance_Track {
