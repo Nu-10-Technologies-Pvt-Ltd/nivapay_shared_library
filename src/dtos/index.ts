110d1ccd55220a352564f74e1f84d6857943e351
gokulvnu10 <132879587+gokulvnu10@users.noreply.github.com>
2023-08-28 10:40:16 +0530

    Update index.ts

--- src/dtos/index.ts
+++ src/dtos/index.ts
@@ -1,8 +1,12 @@
 import { KmsWalletCreateDto } from "./kmsWalletCreate.dto"
+import { SdkRampOrderUpdateDto } from "./sdk-ramp-order-update.dto"
+import { SdkWithdrawalOrderDetailsDto } from "./sdk-withdrawal-order-details.dto"
 import { UpdateBalanceDTO } from "./update_balance.dto"
 
 
 export{
     KmsWalletCreateDto,
-    UpdateBalanceDTO
+    UpdateBalanceDTO,
+    SdkRampOrderUpdateDto,
+    SdkWithdrawalOrderDetailsDto
 }
