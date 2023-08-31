46b31e2f156151c690bd40e79086e06f78de4b37
gokulvnu10 <132879587+gokulvnu10@users.noreply.github.com>
2023-08-31 12:34:25 +0530

    index changes

--- src/fetch_nivapay_services.ts
+++
@@ -1,1 +0,0 @@
-export * as nivapy_product from './fetch_nivapay_services/services/nivapay_products'
--- src/fetch_nivapay_services/index.ts
+++ src/fetch_nivapay_services/index.ts
@@ -1,1 +1,3 @@
-export * from './services/nivapay_products'
+export * as nivapay_product from './services/nivapay_products';
+
+export * as nivapay_core from './services/core';
