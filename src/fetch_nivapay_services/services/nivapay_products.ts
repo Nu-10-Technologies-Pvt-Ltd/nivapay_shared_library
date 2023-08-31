c51477471240623e19fd64802059e4b2dd64647d
gokulvnu10 <132879587+gokulvnu10@users.noreply.github.com>
2023-08-31 14:10:00 +0530

    Update nivapay_products.ts

--- src/fetch_nivapay_services/services/nivapay_products.ts
+++ src/fetch_nivapay_services/services/nivapay_products.ts
@@ -1,6 +1,8 @@
 import axios, { AxiosResponse } from 'axios';
+import * as Config from '../../config';
 
-export async function getProductIdByName (host: string, api_key: string, query: {productName: string}): Promise<AxiosResponse<any>> {
+export async function getProductIdByName (env: string, api_key: string, query: {productName: string}): Promise<AxiosResponse<any>> {
+    const host = Config.default.hosts.core_service[env];
     return await axios.get(
         `${host}/nivapay/products/id-by-name/${query.productName}`,
         {
