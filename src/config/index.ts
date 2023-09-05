d70793fc51a2891b75edeea4fdb82a7a4e6e50d6
sushantgawai <sushantg@nu10.co>
2023-09-05 16:57:07 +0530

    host port number got changed for shared library

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,14 +1,14 @@
 export default {
-    hosts:{
-        core_service:{
+    hosts: {
+        core_service: {
             local: "http://localhost:4000",
             dev: "https://dev-api-core-service.nivapay.com",
             prod: "https://api-core-service.nivapay.com"
         },
-        third_party_service:{
+        third_party_service: {
             local: "http://localhost:3100",
             // dev: "https://dev-api-thirdparty-service.nivapay.com",
-            dev: "http://nivapay-main-api-lb2-77118926.us-east-1.elb.amazonaws.com:3100",
+            dev: "http://nivapay-main-api-lb2-77118926.us-east-1.elb.amazonaws.com:5004",
             prod: "https://api-thirdparty-service.nivapay.com"
         }
     }
--- src/fetch_nivapay_services/services/third_party/wallet.ts
+++ src/fetch_nivapay_services/services/third_party/wallet.ts
@@ -1,11 +1,11 @@
 import axios from 'axios';
 import * as Config from '../../../config';
 
-export async function createRampWallet (
-        env: string, 
-        api_key: string, 
-        query: {currency: string, network: string, userId: string, merchantId: string, testnet: boolean, is_merchant: boolean}
-    ) {
+export async function createRampWallet(
+    env: string,
+    api_key: string,
+    query: { currency: string, network: string, userId: string, merchantId: string, testnet: boolean, is_merchant: boolean }
+) {
 
     const host = Config.default.hosts.third_party_service[env];
     const response = await axios.post(
@@ -22,9 +22,9 @@ export async function createRampWallet (
 }
 
 export async function getKmsWalletRepoByUser(
-    env: string, 
-    api_key: string, 
-    query: {merchant_id: string, userId: string, crypto: string, network: string}
+    env: string,
+    api_key: string,
+    query: { merchant_id: string, userId: string, crypto: string, network: string }
 ) {
     const host = Config.default.hosts.third_party_service[env];
     const response = await axios.post(
@@ -40,22 +40,22 @@ export async function getKmsWalletRepoByUser(
     return response.data
 }
 
-export async function createWallet (
-    env: string, 
-    api_key: string, 
-    query: {currency: string, network: string, userId: string, merchantId: string, testnet: boolean, is_merchant: boolean}
+export async function createWallet(
+    env: string,
+    api_key: string,
+    query: { currency: string, network: string, userId: string, merchantId: string, testnet: boolean, is_merchant: boolean }
 ) {
 
-const host = Config.default.hosts.third_party_service[env];
-const response = await axios.post(
-    `${host}/kms/address/createWallet`,
-    query,
-    {
-        headers: {
-            'x-api-key': api_key,
-        },
-    }
-)
+    const host = Config.default.hosts.third_party_service[env];
+    const response = await axios.post(
+        `${host}/kms/address/createWallet`,
+        query,
+        {
+            headers: {
+                'x-api-key': api_key,
+            },
+        }
+    )
 
-return response.data ? response.data : null
+    return response.data ? response.data : null
 }
