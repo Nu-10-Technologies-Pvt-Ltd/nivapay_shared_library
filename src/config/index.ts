096a8f77b214f91f7419ddf96f7311fbb1b2dc5f
gokulvnu10 <132879587+gokulvnu10@users.noreply.github.com>
2023-09-07 15:11:13 +0530

    Update index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,12 +1,12 @@
 export default {
     hosts: {
         core_service: {
-            local: "http://192.168.0.197:4000",
+            local: "http://localhost:4000",
             dev: "http://dev-api-core-service.nivapay.com",
             prod: "https://api-core-service.nivapay.com"
         },
         third_party_service: {
-            local: "http://192.168.0.197:3100",
+            local: "http://localhost:3100",
             // dev: "https://dev-api-thirdparty-service.nivapay.com",
             dev: "http://nivapay-main-api-lb2-77118926.us-east-1.elb.amazonaws.com:5004",
             prod: "https://api-thirdparty-service.nivapay.com"
