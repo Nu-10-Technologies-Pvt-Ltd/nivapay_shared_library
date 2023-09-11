5d48616baa17949be32683a04e0b62fb4678ad7f
gokulvnu10 <132879587+gokulvnu10@users.noreply.github.com>
2023-09-11 13:19:30 +0530

    added usdt eth inter communication functions

--- src/fetch_nivapay_services/services/third_party/index.ts
+++ src/fetch_nivapay_services/services/third_party/index.ts
@@ -3,3 +3,5 @@ export * as wallet from './wallet'
 export * as tatum_btc from './tatum_btc'
 
 export * as tatum_eth from './tatum_eth'
+
+export * as tatum_usdt from './tatum_usdt_eth'
---
+++ src/fetch_nivapay_services/services/third_party/tatum_usdt_eth.ts
@@ -0,0 +1,38 @@
+import axios from 'axios';
+import * as Config from '../../../config';
+
+export async function getTransactionDetailsByHash(
+    env: string,
+    api_key: string,
+    query: { hash: string, testnet: boolean }
+) {
+
+    const host = Config.default.hosts.third_party_service[env];
+    const response = await axios.get(
+        `${host}/provider/tatum/usdt-eth/transaction/byhash/${query.testnet}/${query.hash}`,
+        {
+            headers: {
+                'x-api-key': api_key,
+            },
+        }
+    )
+    return response.data ? response.data : null
+}
+
+export async function getTransactionByAddress(
+    env: string,
+    api_key: string,
+    query: { wallet_address: string, testnet: boolean, contract_address: string }
+) {
+
+    const host = Config.default.hosts.third_party_service[env];
+    const response = await axios.get(
+        `${host}/provider/tatum/usdt-eth/transaction/byaddress/${query.testnet}/${query.wallet_address}/${query.contract_address}`,
+        {
+            headers: {
+                'x-api-key': api_key,
+            },
+        }
+    )
+    return response.data ? response.data : null
+}
