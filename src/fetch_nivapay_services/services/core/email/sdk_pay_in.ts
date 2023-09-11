fff04204b3443a9035077aad635234d2bd7409df
sushantgawai <sushantg@nu10.co>
2023-09-11 16:26:23 +0530

    detecting and failure emails added

--- src/fetch_nivapay_services/services/core/email/sdk_pay_in.ts
+++ src/fetch_nivapay_services/services/core/email/sdk_pay_in.ts
@@ -2,7 +2,7 @@ import axios, { AxiosResponse } from 'axios';
 import * as Config from '../../../../config';
 import { SdkWithdrawalOrderDetailsDto } from '../../../../dtos/dtos/sdk-withdrawal-order-details.dto';
 
-export async function error (env: string, api_key: string, query: { payload: SdkWithdrawalOrderDetailsDto, eta: string}) {
+export async function error(env: string, api_key: string, query: { payload: SdkWithdrawalOrderDetailsDto, eta: string }) {
     const host = Config.default.hosts.core_service[env];
     const response = await axios.post(
         `${host}/sdk-payin-mail/send-error-email`,
@@ -19,7 +19,7 @@ export async function error (env: string, api_key: string, query: { payload: Sdk
     return response.data
 }
 
-export async function session_time_out (env: string, api_key: string, query: { payload: SdkWithdrawalOrderDetailsDto}) {
+export async function session_time_out(env: string, api_key: string, query: { payload: SdkWithdrawalOrderDetailsDto }) {
     const host = Config.default.hosts.core_service[env];
     const response = await axios.post(
         `${host}/sdk-payin-mail/session-timed-out`,
@@ -33,7 +33,7 @@ export async function session_time_out (env: string, api_key: string, query: { p
     return response.data
 }
 
-export async function success (env: string, api_key: string, query: { payload: SdkWithdrawalOrderDetailsDto}) {
+export async function success(env: string, api_key: string, query: { payload: SdkWithdrawalOrderDetailsDto }) {
     const host = Config.default.hosts.core_service[env];
     const response = await axios.post(
         `${host}/sdk-payin-mail/success`,
@@ -47,10 +47,27 @@ export async function success (env: string, api_key: string, query: { payload: S
     return response.data
 }
 
-export async function processing (env: string, api_key: string, query: { payload: SdkWithdrawalOrderDetailsDto, eta: string}) {
+export async function detecting(env: string, api_key: string, query: { payload: SdkWithdrawalOrderDetailsDto, eta: string }) {
     const host = Config.default.hosts.core_service[env];
     const response = await axios.post(
-        `${host}/sdk-payin-mail/processing`,
+        `${host}/sdk-payin-mail/detecting`,
+        {
+            payload: query.payload,
+            eta: query.eta
+        },
+        {
+            headers: {
+                'x-api-key': api_key,
+            },
+        }
+    )
+    return response.data
+}
+
+export async function failure(env: string, api_key: string, query: { payload: SdkWithdrawalOrderDetailsDto, eta: string }) {
+    const host = Config.default.hosts.core_service[env];
+    const response = await axios.post(
+        `${host}/sdk-payin-mail/failure`,
         {
             payload: query.payload,
             eta: query.eta
