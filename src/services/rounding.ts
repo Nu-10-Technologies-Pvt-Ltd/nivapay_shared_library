72270f9c933467b40ed6a8b316cb236e618b3ce3
gokulvnu10 <132879587+gokulvnu10@users.noreply.github.com>
2023-09-14 16:13:26 +0530

    added decimal.js

--- src/services/rounding.ts
+++ src/services/rounding.ts
@@ -1,9 +1,25 @@
-export function roundUpToDecimalPlaces(number: number, decimalPlaces: number): number {
-    const multiplier = Math.pow(10, decimalPlaces);
-    return Math.ceil(number * multiplier) / multiplier;
+import Decimal from "decimal.js";
+
+export function roundUpToDecimalPlaces(numberStr, decimalPlacesStr) {
+    const decimalNumber = new Decimal(parseFloat(numberStr));
+    const decimalPlaces = parseInt(decimalPlacesStr);
+    const rounded = decimalNumber.toFixed(decimalPlaces, Decimal.ROUND_UP);
+    return rounded.toString();
 }
 
-export function roundDownToDecimalPlaces(number: number, decimalPlaces: number): number {
-    const multiplier = Math.pow(10, decimalPlaces);
-    return Math.floor(number * multiplier) / multiplier;
-} 
+export function roundDownToDecimalPlaces(numberStr, decimalPlacesStr) {
+    const decimalNumber = new Decimal(parseFloat(numberStr));
+    const decimalPlaces = parseInt(decimalPlacesStr);
+    const rounded = decimalNumber.toFixed(decimalPlaces, Decimal.ROUND_DOWN);
+    return parseFloat(rounded);
+}
+
+// export function roundUpToDecimalPlaces(number: number, decimalPlaces: number): number {
+//     const multiplier = Math.pow(10, decimalPlaces);
+//     return Math.ceil(number * multiplier) / multiplier;
+// }
+
+// export function roundDownToDecimalPlaces(number: number, decimalPlaces: number): number {
+//     const multiplier = Math.pow(10, decimalPlaces);
+//     return Math.floor(number * multiplier) / multiplier;
+// } 
