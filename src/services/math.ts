445685c8cc5dbddee727f7826b2b2e5101fe78ae
gokulvnu10 <132879587+gokulvnu10@users.noreply.github.com>
2023-09-15 05:39:36 +0530

    added division and return string and number

--- src/services/math.ts
+++ src/services/math.ts
@@ -4,22 +4,40 @@ export function roundUpToDecimalPlaces(numberStr, decimalPlacesStr) {
     const decimalNumber = new Decimal(parseFloat(numberStr));
     const decimalPlaces = parseInt(decimalPlacesStr);
     const rounded = decimalNumber.toFixed(decimalPlaces, Decimal.ROUND_UP);
-    return rounded.toString();
+    return {
+        resultAsString : rounded.toString(),
+        resultAsNumber : parseFloat(rounded)
+    }
 }
 
 export function roundDownToDecimalPlaces(numberStr, decimalPlacesStr) {
     const decimalNumber = new Decimal(parseFloat(numberStr));
     const decimalPlaces = parseInt(decimalPlacesStr);
     const rounded = decimalNumber.toFixed(decimalPlaces, Decimal.ROUND_DOWN);
-    return rounded.toString();
+    return {
+        resultAsString : rounded.toString(),
+        resultAsNumber : parseFloat(rounded)
+    }
 }
 
 export function multiply(numberOne, numberTwo) {
     const currentValue = new Decimal(numberOne);
     const result = currentValue.times(new Decimal(numberTwo));
-    return result.toString();
+    return {
+        resultAsString : result.toString(),
+        resultAsNumber : result.toNumber()
+    }
 }
 
+export function divide(dividendStr, divisorStr){
+    const dividend = new Decimal(dividendStr); 
+    const divisor = new Decimal(divisorStr); 
+    const result = dividend.dividedBy(divisor);
+    return {
+        resultAsString : result.toString(),
+        resultAsNumber : result.toNumber()
+    }
+}
 
 // export function roundUpToDecimalPlaces(number: number, decimalPlaces: number): number {
 //     const multiplier = Math.pow(10, decimalPlaces);
