eee5ca9b16dcf1057aa3b01abe4f5d14db9440fb
gokulvnu10 <132879587+gokulvnu10@users.noreply.github.com>
2023-09-22 15:00:03 +0530

    accounts_accouting entity creation

---
+++ src/entities/core/models/accounts_accounting.entity.ts
@@ -0,0 +1,46 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Accounts_Accounting{
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column()
+    accountId: string;
+
+    @Column({nullable: true})
+    orderId: string;
+
+    @Column()
+    transactionId: string;
+
+    @Column()
+    currencyId: string;
+
+    @Column()
+    balanceBefore: string;
+
+    @Column()
+    credit: string;
+
+    @Column()
+    debit: string;
+
+    @Column()
+    balanceAfter: string;
+
+    @Column({nullable: true})
+    balanceBefore_$: number;
+
+    @Column({nullable: true})
+    credit_$: number;
+
+    @Column({nullable: true})
+    debit_$: number;
+
+    @Column({nullable: true})
+    balanceAfter_$: number;
+
+    @CreateDateColumn({ type: 'timestamptz' })
+    createdAt: Date;
+}
