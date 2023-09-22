535f4c8a50f0fb629944c33dc05b9e8c4bfc8064
sushantgawai <sushantg@nu10.co>
2023-09-22 12:31:46 +0530

    on_chain_transaction entity class added

---
+++ src/entities/common/models/on_chain_transactions.entity.ts
@@ -0,0 +1,40 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity("on-chain-transactions")
+export class onChainTransactions {
+    @PrimaryGeneratedColumn("uuid")
+    transactionId: string;
+
+    @Column()
+    walletId: string;
+
+    @Column()
+    transactionHash: string; // NVP=> merchant, merchant-user, user
+
+    @Column()
+    transactionDirection: string;
+
+    @Column()
+    transactionCurrencyId: string;
+
+    @Column()
+    transactionAmount: string
+
+    @Column()
+    networkFeeCurrencyId: string;
+
+    @Column()
+    networkFeeAmount: string;
+
+    @Column()
+    destinationAddresses: string;
+
+    @Column()
+    senderAddresses: string;
+
+    @Column({ nullable: true })
+    confirmedAt: string;
+
+    @CreateDateColumn({ type: "timestamp" })
+    createdAt: Date;
+}
