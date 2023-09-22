36f9fdb619875f38dda6e49e3c619efdc8045f8b
sushantgawai <sushantg@nu10.co>
2023-09-22 10:58:09 +0530

    cmc module updated

--- src/fetch_nivapay_services/services/third_party/cmc.ts
+++ src/fetch_nivapay_services/services/third_party/cmc.ts
@@ -10,7 +10,7 @@ export async function rateConversion(
 
     const host = Config.default.hosts.third_party_service[env];
     const response = await axios.get(
-        `${host}/cmc/convert?amount=${query.amount}&symbol=${query.symbol}&convertSymbol=${query.convertSymbol}`,
+        `${host}/cmc/convert?amount=${query.amount}&symbol=${query.id}&convertSymbol=${query.convertToId}`,
         {
             headers: {
                 'x-api-key': api_key,
