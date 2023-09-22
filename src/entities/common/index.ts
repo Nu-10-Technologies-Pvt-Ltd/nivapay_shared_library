3dee76ffef7aad41f572888ac8e43492ac6b1555
sushantgawai <sushantg@nu10.co>
2023-09-22 12:31:52 +0530

    on_chain_transaction entity class added

--- src/entities/common/index.ts
+++ src/entities/common/index.ts
@@ -1,14 +1,23 @@
+import { accountWalletAddress } from "./models/account_wallet_address.entity";
+import { accounts } from "./models/accounts.entity";
 import { M_Balance } from "./models/m_balance.entity";
 import { M_Balance_Track } from "./models/m_balance_track.entity";
+import { onChainTransactions } from "./models/on_chain_transactions.entity";
 
 const Common_Entities = [
     M_Balance,
-    M_Balance_Track
+    M_Balance_Track,
+    onChainTransactions,
+    accounts,
+    accountWalletAddress
 ]
 
 export {
     M_Balance,
-    M_Balance_Track
+    M_Balance_Track,
+    onChainTransactions,
+    accounts,
+    accountWalletAddress
 }
 
 export default Common_Entities;
