5bc32974a43151104c54d9dd84696d8fd887a8f5
sushantgawai <sushantg@nu10.co>
2023-09-26 09:39:52 +0530

    exponetial values function added

--- src/services/math.ts
+++ src/services/math.ts
@@ -1,14 +1,14 @@
 import Decimal from "decimal.js";
-import bigDecimal from "js-big-decimal";
+import { BigNumber } from 'bignumber.js'
 
 export function roundUpToDecimalPlaces(numberIp, decimalPlacesIp) {
     const decimalNumber = new Decimal(parseFloat(numberIp));
     const decimalPlaces = parseInt(decimalPlacesIp);
     const rounded = decimalNumber.toFixed(decimalPlaces, Decimal.ROUND_UP);
     return {
-        resultAsString : rounded.toString(),
-        resultAsNumber : parseFloat(rounded),
-        resultAsNumberString : `${parseFloat(rounded)}`
+        resultAsString: rounded.toString(),
+        resultAsNumber: parseFloat(rounded),
+        resultAsNumberString: `${parseFloat(rounded)}`
     }
 }
 
@@ -17,18 +17,18 @@ export function roundDownToDecimalPlaces(numberIp, decimalPlacesIp) {
     const decimalPlaces = parseInt(decimalPlacesIp);
     const rounded = decimalNumber.toFixed(decimalPlaces, Decimal.ROUND_DOWN);
     return {
-        resultAsString : rounded.toString(),
-        resultAsNumber : parseFloat(rounded),
-        resultAsNumberString : `${parseFloat(rounded)}`
+        resultAsString: rounded.toString(),
+        resultAsNumber: parseFloat(rounded),
+        resultAsNumberString: `${parseFloat(rounded)}`
     }
 }
 
-export function roundUpToNearestInteger(numberIp){
+export function roundUpToNearestInteger(numberIp) {
     const decimalNumber = new Decimal(numberIp);
     const rounded = decimalNumber.ceil();
     return {
-        resultAsString : rounded.toString(),
-        resultAsNumber : rounded.toNumber()
+        resultAsString: rounded.toString(),
+        resultAsNumber: rounded.toNumber()
     }
 }
 
@@ -36,37 +36,44 @@ export function multiply(numberOne, numberTwo) {
     const currentValue = new Decimal(numberOne);
     const result = currentValue.times(new Decimal(numberTwo));
     return {
-        resultAsString : result.toString(),
-        resultAsNumber : result.toNumber()
+        resultAsString: result.toString(),
+        resultAsNumber: result.toNumber()
     }
 }
 
-export function divide(dividendIp, divisorIp){
-    const dividend = new Decimal(dividendIp); 
-    const divisor = new Decimal(divisorIp); 
+export function divide(dividendIp, divisorIp) {
+    const dividend = new Decimal(dividendIp);
+    const divisor = new Decimal(divisorIp);
     const result = dividend.dividedBy(divisor);
     return {
-        resultAsString : result.toString(),
-        resultAsNumber : result.toNumber()
+        resultAsString: result.toString(),
+        resultAsNumber: result.toNumber()
     }
 }
 
-export function addition(numberOne, numberTwo){
+export function addition(numberOne, numberTwo) {
     const decimalOne = new Decimal(numberOne);
     const decimalTwo = new Decimal(numberTwo);
     const result = decimalOne.plus(decimalTwo);
     return {
-        resultAsString : result.toString(),
-        resultAsNumber : result.toNumber()
+        resultAsString: result.toString(),
+        resultAsNumber: result.toNumber()
     }
 }
 
-export function subtraction(numberOne, numberTwo){
+export function subtraction(numberOne, numberTwo) {
     const decimalOne = new Decimal(numberOne);
     const decimalTwo = new Decimal(numberTwo);
     const result = decimalOne.minus(decimalTwo);
     return {
-        resultAsString : result.toString(),
-        resultAsNumber : result.toNumber()
+        resultAsString: result.toString(),
+        resultAsNumber: result.toNumber()
     }
+}
+
+
+export function scientificToDecimal(scientificNotation) {
+    const bigNumber = new BigNumber(scientificNotation);
+    const decimalNumber = bigNumber.toFormat(Math.abs(bigNumber.e))
+    return decimalNumber
 }
