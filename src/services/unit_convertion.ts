6c6d48f21c3564bd8ef6a8015df9ef70b0d5831c
gokulvnu10 <132879587+gokulvnu10@users.noreply.github.com>
2023-10-05 16:24:43 +0530

    added sathoshis_to_BTC unit conversion function

--- src/services/unit_convertion.ts
+++ src/services/unit_convertion.ts
@@ -1,7 +1,14 @@
 import Units from 'ethereumjs-units'
 import Decimal from 'decimal.js';
+import { divide } from './math';
 
 export function eth_convert(value, unitFrom, unitTo){
     const decimalNumber = new Decimal(value);
     return Units.convert(decimalNumber, unitFrom, unitTo)
+}
+
+export function sathoshis_to_BTC(value){
+    const sathoshi = new Decimal(value);
+    const divisor = new Decimal('100000000')
+    return divide(sathoshi, divisor)
 }
