e96226925f2bc5378cb6495fb13b9edd481490c4
sushantgawai <sushantg@nu10.co>
2023-10-06 12:54:32 +0530

    on ramp trasure swap entities addingg

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ export * from './entities/common'
 export * from './entities/core'
 export * from './entities/on_ramp'
 export * from './entities/user_deposits'
+export * from "./entities/on_treasury_swap"
 
 export * from './dtos'
 
