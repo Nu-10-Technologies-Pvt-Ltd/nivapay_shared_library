80554441916c9f0cc357ef6c84505dee7e1e8b56
gokulvnu10 <132879587+gokulvnu10@users.noreply.github.com>
2023-10-31 22:55:16 +0530

    Create create_swap_order.dto.ts

---
+++ src/dtos/dtos/swap_component/create_swap_order.dto.ts
@@ -0,0 +1,25 @@
+import { IsNotEmpty, IsString } from "class-validator";
+
+export class createSwapOrderDto {
+    order_status?: string;
+    
+    @IsNotEmpty()
+    @IsString()
+    swap_in_currency_id: string;
+
+    @IsNotEmpty()
+    @IsString()
+    swap_in_wallet_id: string;
+
+    @IsNotEmpty()
+    @IsString()
+    swap_in_amount: string;
+
+    @IsNotEmpty()
+    @IsString()
+    swap_out_currency_id: string;
+
+    @IsNotEmpty()
+    @IsString()
+    swap_out_wallet_address: string;
+}
