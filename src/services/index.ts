270f8ea1591b779d3e8a8ba25be2895fbe14e099
gokulvnu10 <132879587+gokulvnu10@users.noreply.github.com>
2023-11-07 17:06:52 +0530

    added fee_calculations

--- src/services/index.ts
+++ src/services/index.ts
@@ -5,3 +5,5 @@ export * as generate_random from './generate_random'
 export * as unit_conversion from './unit_convertion'
 
 export * as date_time from './date_time'
+
+export * as fee_calculation from './fee_calculation'
