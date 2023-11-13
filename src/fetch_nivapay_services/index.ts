1db9d4c77f13b351043b1125351200bc048dddba
sushantgawai <sushantg@nu10.co>
2023-11-13 13:16:29 +0530

    exporting ramp order

--- src/fetch_nivapay_services/index.ts
+++ src/fetch_nivapay_services/index.ts
@@ -3,3 +3,5 @@ export * as core from './services/core'
 export * as third_party from './services/third_party'
 
 export * as user_deposits from './services/user_deposits'
+
+export * as on_ramp from './services/onramp_component'
