da6ad1d0df71055874a36a8455f42378aa974dc3
gokulvnu10 <132879587+gokulvnu10@users.noreply.github.com>
2023-11-15 16:59:36 +0530

    Order entity and dto

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ export * from './entities/on_ramp'
 export * from './entities/user_deposits'
 export * from "./entities/on_treasury_swap"
 export * from "./entities/swap_component"
+export * from "./entities/deposit_component"
 
 export * from './dtos'
 
