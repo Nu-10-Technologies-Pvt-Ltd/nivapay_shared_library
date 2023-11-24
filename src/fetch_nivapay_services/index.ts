39089ceed52fcf24eb6f45afb9443e1259b8e4d9
sushantgawai <sushantg@nu10.co>
2023-11-24 14:50:25 +0530

    exporting deposit apis

--- src/fetch_nivapay_services/index.ts
+++ src/fetch_nivapay_services/index.ts
@@ -1,9 +1,11 @@
-export * as core from './services/core'
+export * as core from './services/core';
 
-export * as third_party from './services/third_party'
+export * as third_party from './services/third_party';
 
-export * as user_deposits from './services/user_deposits'
+export * as user_deposits from './services/user_deposits';
 
-export * as on_ramp from './services/onramp_component'
+export * as on_ramp from './services/onramp_component';
 
-export * as swap_component from './services/swap_component'
+export * as swap_component from './services/swap_component';
+
+export * as deposit_process from './services/deposit_component';
