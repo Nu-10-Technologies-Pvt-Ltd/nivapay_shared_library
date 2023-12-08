653776f4fed2192d7086859582cb4fbf956cfbad
gokulvnu10 <132879587+gokulvnu10@users.noreply.github.com>
2023-12-08 15:08:51 +0530

    added network fee calculation function

--- src/services/fee_calculation.ts
+++ src/services/fee_calculation.ts
@@ -1,3 +1,4 @@
+import { FetchNivapayServices } from '..';
 import {multiply,divide} from './math';
 
 export function service_fee(service_fee_config, transaction_amount) {
@@ -9,5 +10,26 @@ export function service_fee(service_fee_config, transaction_amount) {
     }
     else if(service_fee_config.fee_unit === "absolute"){
         return service_fee_config.fee_value
+    }
+}
+
+export async function network_fee(networkFeeAmount: string, networkFeeCurrencyId: string, transaction_currency_id: string){
+    if(networkFeeCurrencyId.toLowerCase() === transaction_currency_id.toLowerCase()){
+        return networkFeeAmount
+    }
+    else{
+        const to_transaction_currency = (
+            await FetchNivapayServices.third_party.cmc.rateConversion(
+                this.env,
+                this.i_api_key,
+                {
+                    amount: Number(networkFeeAmount),
+                    id: networkFeeCurrencyId,
+                    convertToId: transaction_currency_id
+                },
+            )
+        ).data.price;
+
+        return to_transaction_currency
     }
 }
