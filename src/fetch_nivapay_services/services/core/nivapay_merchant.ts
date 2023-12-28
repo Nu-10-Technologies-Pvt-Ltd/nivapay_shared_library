c8ed6e13a7a8a24beadae2a78d2ec71c293afbca
gokulvnu10 <132879587+gokulvnu10@users.noreply.github.com>
2023-12-28 09:15:10 +0530

    added getOrCreateSelfTxnDepositWallet

--- src/fetch_nivapay_services/services/core/nivapay_merchant.ts
+++ src/fetch_nivapay_services/services/core/nivapay_merchant.ts
@@ -14,3 +14,21 @@ export async function getById (env: string, api_key: string, query: {id: string}
     )
     return response.data
 }
+
+export async function getOrCreateSelfTxnDepositWallet (env: string, api_key: string, query: {
+    merchant_id: string,
+    currency_id: string,
+    merchant_nick_name?: string
+}) {
+    const host = Config.default.hosts.core_service[env];
+    const response = await axios.post(
+        `${host}/merchant/self-txn/address/getorcreate/deposit`,
+        query,
+        {
+            headers: {
+                'x-api-key': api_key,
+            },
+        }
+    )
+    return response.data
+}
