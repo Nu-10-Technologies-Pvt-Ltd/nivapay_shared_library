53f5d89186c9489d17882397fb0df7c393bc6481
unknown <raghunathk@nu10.co>
2024-01-03 11:51:08 +0530

    sc-transactions table added

---
+++ src/entities/sc-component/index.ts
@@ -0,0 +1,11 @@
+import { ScTransactionsModel } from "./models/sc_transactions.entity"
+
+const sc_component_entities = [
+    ScTransactionsModel,
+]
+
+export {
+    ScTransactionsModel,
+}
+
+export default sc_component_entities
