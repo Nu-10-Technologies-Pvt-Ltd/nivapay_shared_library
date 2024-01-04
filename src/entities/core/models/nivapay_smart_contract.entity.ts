4783814bce56b4d12e90d6ba0297516ba0d2b8e8
gokulvnu10 <132879587+gokulvnu10@users.noreply.github.com>
2024-01-04 12:10:32 +0530

    update schema

--- src/entities/core/models/nivapay_smart_contract.entity.ts
+++ src/entities/core/models/nivapay_smart_contract.entity.ts
@@ -8,23 +8,41 @@ export class NivapaySmartContractModel {
     @Column({comment: "Smart Contract id"})
     id: string;
 
+    @Column({comment: "A name to identify this contract"})
+    sc_display_name: string;
+
+    @Column({comment: "Action done by the smart contract"})
+    sc_action: string;
+
     @Column({comment: "Address of smart contract"})
     sc_address: string;
 
     @Column({comment: "Nivapay network id"})
     network_id: string;
-
+    
     @Column({comment: "Name of the smart contract method"})
     method_name: string;
-
+    
     @Column({comment: "Signature of the smart contract method"})
     method_signature: string;
-
+    
     @Column({type: 'jsonb', comment: "ABI (Application Binary Interface) definition for the smart contract method"})
     method_ABI: any;
+    
+    @Column({nullable: true})
+    method_definition: string;
 
     @Column({comment: "Maximum amount of gas allowed for executing the smart contract method"})
     gas_limit: string;
+
+    @Column({nullable: true, comment: "Name of parameter in ABI which indicates the total number of NFT's to be issued"})
+    sc_FunctionTokenCountParam: string;
+
+    @Column({nullable: true, comment: "Name of the token, If a token is being issued by this contract method"})
+    issueTokenName: string;
+
+    @Column({nullable: true, comment: "Symbol of the token, If a token is being issued by this contract method"})
+    issueTokenSymbol: string;
 
     @Column({comment: "Enable status in Nivapay for this contract"})
     isEnabled: boolean;
