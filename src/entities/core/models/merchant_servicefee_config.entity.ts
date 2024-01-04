5f71e0d1d76e22dada52cd831f77cd3045e9442c
gokulvnu10 <132879587+gokulvnu10@users.noreply.github.com>
2024-01-04 12:37:56 +0530

    Update merchant_servicefee_config.entity.ts

--- src/entities/core/models/merchant_servicefee_config.entity.ts
+++ src/entities/core/models/merchant_servicefee_config.entity.ts
@@ -10,8 +10,11 @@ export class MerchantServiceFeeConfigurationModel {
     @PrimaryGeneratedColumn('uuid')
     id: string;
   
-    @Column()
+    @Column({nullable: true})
     virtual_currency_config_id: string
+
+    @Column({nullable: true})
+    smart_contract_config_id: string
   
     @Column()
     fee_type: string 
