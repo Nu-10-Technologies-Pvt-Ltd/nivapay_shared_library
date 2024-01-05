d13d243834fb3045c184f2ede22db9bd938158c7
gokulvnu10 <132879587+gokulvnu10@users.noreply.github.com>
2024-01-05 13:25:53 +0530

    Update invoke_smart_contract.dto.ts

--- src/dtos/dtos/third_party/tatum_smart_contract/invoke_smart_contract.dto.ts
+++ src/dtos/dtos/third_party/tatum_smart_contract/invoke_smart_contract.dto.ts
@@ -4,13 +4,13 @@ export class InvokeSmartContractDto{
     @IsString()
     nivapay_network_id: string;
 
-    data: InvokeSmartContractData
-}
-
-export class InvokeSmartContractData{
     contractAddress: string;
+
     methodName: string;
+
     methodABI: any;
+
     params: any;
-    fromPrivateKey: string;
+
+    fromWalletId: string;
 }
