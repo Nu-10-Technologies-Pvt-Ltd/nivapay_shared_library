6b6a35ba00c1aff49a085f126273cf819b72d8bb
unknown <sushantg@nu10.co>
2024-02-02 12:35:42 +0530

    sc transaction details api bug fixes

--- src/fetch_nivapay_services/services/sc_component/sc.ts
+++ src/fetch_nivapay_services/services/sc_component/sc.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import * as Config from '../../../config';
 
-export async function initiateOrder (env: string, api_key: string, query: {order_id: string}) {
+export async function initiateOrder(env: string, api_key: string, query: { order_id: string }) {
     const host = Config.default.hosts.sc_component[env];
     const response = await axios.get(
         `${host}/smart_contract/initiate/order/${query.order_id}`,
@@ -14,8 +14,8 @@ export async function initiateOrder (env: string, api_key: string, query: {order
     return response.data;
 }
 
-export async function getTransactionDetails (env: string, api_key: string, query: {order_id: string}) {
-    const host = Config.default.hosts.swap_component[env];
+export async function getTransactionDetails(env: string, api_key: string, query: { order_id: string }) {
+    const host = Config.default.hosts.sc_component[env];
     const response = await axios.get(
         `${host}/accounting/transaction/details/${query.order_id}`,
         {
