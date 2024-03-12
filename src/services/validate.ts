d05c7c6cbcf7286f9481203b7dfe0b874a34316a
gokulvnu10 <132879587+gokulvnu10@users.noreply.github.com>
2024-03-12 23:40:34 +0530

    create payout wallet dto
    is wallet address valid function created
    custom validator created

---
+++ src/services/validate.ts
@@ -0,0 +1,11 @@
+import * as validateWallet from 'validate-wallet';
+
+export function validate_wallet_address(network: string, address: string): boolean {
+    try {
+        console.log('inside validate ######')
+        return validateWallet.validate(network, address);
+    } catch (error) {
+        console.error('Error occurred while validating wallet address:', error.message);
+        return false;
+    }
+}
