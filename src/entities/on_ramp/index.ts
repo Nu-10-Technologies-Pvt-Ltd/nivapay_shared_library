ce26c62957e58c998abee556e8b98f6bd24025dc
Pavan-akhil <pavana@nu10.co>
2024-03-25 15:13:09 +0530

    webhook event added

--- src/entities/on_ramp/index.ts
+++ src/entities/on_ramp/index.ts
@@ -2,19 +2,22 @@ import { OnrampOrder } from "./models/create-onramp-order.entity";
 import { OnrampOrderAccounting } from "./models/onrampOrderAccounting.entity";
 import { OnRampTreasuryTransactionModel } from "./models/ramp_treasury_transaction.entity";
 import { TransakWebHookEvent } from "./models/transak_webhook_events.entity";
+import { RampWebHookEvent } from "./models/webhook.event.entity";
 
 const OnRamp_Entities = [
     TransakWebHookEvent,
     OnrampOrderAccounting,
     OnrampOrder,
-    OnRampTreasuryTransactionModel
+    OnRampTreasuryTransactionModel,
+    RampWebHookEvent
 ]
 
 export {
     TransakWebHookEvent,
     OnrampOrderAccounting,
     OnrampOrder,
-    OnRampTreasuryTransactionModel
+    OnRampTreasuryTransactionModel,
+    RampWebHookEvent
 }
 
 export default OnRamp_Entities;
